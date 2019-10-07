9d68eab5d28e81cb351d5d5fccb9c3387f4fc47b
Darpan Goyal <darpangoyal@Darpans-MacBook-Pro.local>
2019-10-06 17:24:59 -0700

    deleting old stuff

--- fullstack_app/client/src/index.js
+++
@@ -1,9 +0,0 @@
-// client/src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();

638224da128f7f2993cbb3713e68f20be25bb380
Kevin Kim <happylicious.kevin@gmail.com>
2019-11-03 00:20:23 -0700

    minor update to model

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
     name: { type: String },
-    college: { type: String },
-    majors: { type: [String], },
+    college: { type: Schema.Types.ObjectId, ref: 'College' },
+	majors: [{ type: Schema.Types.ObjectId, ref: 'Major' }],
     description: { type: String, default: "Project description goes here." },
     skills: { type: [String], default: ["Skills go here."] },
     creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },

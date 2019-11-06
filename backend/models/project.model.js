0ecb918b80cf99a891e43797c3af504b23e69c00
Kevin Kim <happylicious.kevin@gmail.com>
2019-11-05 23:34:58 -0800

    changed skills data type to an array of objectIds

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
     name: { type: String },
+    description: { type: String },
     college: { type: Schema.Types.ObjectId, ref: 'College' },
 	majors: [{ type: Schema.Types.ObjectId, ref: 'Major' }],
-    description: { type: String, default: "Project description goes here." },
-    skills: { type: [String], default: ["Skills go here."] },
+    skills: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     peers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,12 +7,12 @@ const userSchema = new Schema({
 	password: { type: String, required: true },
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
-	biography: { type: String, default: "Your biography goes here." },
+	biography: { type: String },
 	college: { type: Schema.Types.ObjectId, ref: 'College' },
 	major: { type: Schema.Types.ObjectId, ref: 'Major' },
-	skills: { type: [String], default: ["Your skills go here."] },
-	picture: { type: String, default: "/profile/profile.png" },
-	resume: { type: String, default: "/resume/resume.pdf" },
+	skills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }],
+	picture: { type: String },
+	resume: { type: String },
 });
 
 const User = mongoose.model('User', userSchema);

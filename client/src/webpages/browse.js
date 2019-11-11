404f661c3b4e04e568f0429c558ff61b2e30ef81
Kevin Kim <happylicious.kevin@gmail.com>
2019-11-11 02:07:49 -0800

    added filter toggle and some ability to view projects

--- client/src/webpages/browse.js
+++ client/src/webpages/browse.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { Button, ControlLabel, FormGroup, ListGroup, ListGroupItem, PageHeader, Grid, Row, Col } from "react-bootstrap";
+import { Button, Col, ControlLabel, FormGroup, Grid, ListGroup, ListGroupItem, PageHeader, Panel, Row } from "react-bootstrap";
 import Select from 'react-select';
 import "../styles/browse.css";
 
@@ -14,6 +14,9 @@ export default function Projects(props) {
   const [skillList, setSkillList] = useState([]);
 
   const [projectList, setProjectList] = useState([]);
+  const [project, setProject] = useState([]);
+
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     axios.get('http://localhost:3000/college')
@@ -27,9 +30,24 @@ export default function Projects(props) {
     axios.get('http://localhost:3000/skill')
       .then(res => { setSkillList(res.data) })
       .catch(error => console.log(error));
+
+    axios.get('http://localhost:3000/project')
+      .then(res => { setProjectList(res.data) })
+      .catch(error => console.log(error));
   }, []); 
 
-  function handleSubmit(event) {
+  function toggles() {
+    setProject([]);
+    setToggle(!toggle);
+
+    if (toggle) {
+      document.getElementById('toggle').innerText = 'Filter';
+    } else {
+      document.getElementById('toggle').innerText = 'Apply';
+    }
+  }
+
+  function search(event) {
     event.preventDefault();
 
     const searchData = {
@@ -39,68 +57,104 @@ export default function Projects(props) {
     };
 
     axios.post('http://localhost:3000/project/search', searchData)
-      .then(res=> { setProjectList(res.data) })
+      .then(res => { setProjectList(res.data) })
       .catch(error => console.log(error));
+  }
+
+  function displayProjects() {
+    return (projectList.map(project => 
+      <ListGroupItem 
+        header={project.name}
+        onClick={()=>setProject(project)}
+      > 
+        {project.description}
+      </ListGroupItem>
+    ));
+  }
+
+  // still needs work
+  function viewProject() {
+    return (
+      <p>{project.name}</p>
+    );
   }
 
   return (
     <div className="Browse">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={search}>
         <PageHeader>
           Browse
+          <Button id="toggle" className="pull-right" type="submit" onClick={toggles}>
+            Filter
+          </Button>
         </PageHeader>
-        <Grid>
-          <Row>
-            <Col xs={6}>
-              <FormGroup bsSize="large">
-                <ControlLabel>Filter by college</ControlLabel>
-                <Select className="select"
-                  isClearable
-                  value={college}
-                  options={collegeList}
-                  getOptionLabel = {(option)=>option.name}
-                  getOptionValue = {(option)=>option._id}
-                  onChange={e => setCollege(e)}
-                />
-              </FormGroup>
-            </Col>
-            <Col xs={6}>
-              <FormGroup bsSize="large">
-                <ControlLabel>Filter by major</ControlLabel>
-                <Select className="select"
-                  isClearable
-                  value={major}
-                  options={majorList}
-                  getOptionLabel = {(option)=>option.name}
-                  getOptionValue = {(option)=>option._id}
-                  onChange={e => setMajor(e)}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <FormGroup bsSize="large">
-                <ControlLabel>Filter by skills</ControlLabel>
-                <Select className="select"
-                  isMulti
-                  value={skills}
-                  options={skillList}
-                  getOptionLabel = {(option)=>option.name}
-                  getOptionValue = {(option)=>option._id}
-                  onChange={e => setSkills(e)}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-        </Grid>
-        <Button bsStyle="primary" bsSize="large" block type="submit">
-          Search
-        </Button>
+        <Panel className="filter" expanded={toggle}>
+          <Panel.Collapse>
+            <Grid>
+              <Row>
+                <Col xs={6}>
+                  <FormGroup bsSize="large">
+                    <ControlLabel>Filter by college</ControlLabel>
+                    <Select 
+                      className="select"
+                      isClearable
+                      value={college}
+                      options={collegeList}
+                      getOptionLabel = {(option)=>option.name}
+                      getOptionValue = {(option)=>option._id}
+                      onChange={e => setCollege(e)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col xs={6}>
+                  <FormGroup bsSize="large">
+                    <ControlLabel>Filter by major</ControlLabel>
+                    <Select 
+                      className="select"
+                      isClearable
+                      value={major}
+                      options={majorList}
+                      getOptionLabel = {(option)=>option.name}
+                      getOptionValue = {(option)=>option._id}
+                      onChange={e => setMajor(e)}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12}>
+                  <FormGroup bsSize="large">
+                    <ControlLabel>Filter by skills</ControlLabel>
+                    <Select 
+                      className="select"
+                      isMulti
+                      value={skills}
+                      options={skillList}
+                      getOptionLabel = {(option)=>option.name}
+                      getOptionValue = {(option)=>option._id}
+                      onChange={e => setSkills(e)}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Grid>
+          </Panel.Collapse>
+        </Panel>
       </form>
-      <ListGroup>
-        {projectList.map(project => <ListGroupItem onClick={() => console.log(project.name)}>{project.name}</ListGroupItem>)}
-      </ListGroup>
+      <Grid>
+        <Row>
+        <Col xs={6}>
+          <ListGroup>
+            {displayProjects()}
+          </ListGroup>
+        </Col>
+        <Col xs={6}>
+          <ListGroup>
+            {viewProject()}
+          </ListGroup>
+        </Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
